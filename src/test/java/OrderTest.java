51152b631f035318fc7e9ca126e4b85ce2ab4837
Andrey Turchak <andrewturchak78@mail.ru>
2023-03-29 21:51:46 +0300

    setUpAll added

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,6 +10,7 @@ public class OrderTest {
 
     @BeforeAll
     static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
         System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
     }
     @BeforeEach
